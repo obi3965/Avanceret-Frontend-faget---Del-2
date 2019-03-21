0b69a191f19470ae29f401319c2275be12991dac
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-03-21 10:12:43 +0100

    commit

--- del2.js
+++ del2.js
@@ -1,6 +1,8 @@
-window.addEventListener('load', function(){
+
 
     (function(){
+
+    
         var anchor = document.getElementById('anchor');
         anchor.onclick = function(){
             var xhr = new XMLHttpRequest();
@@ -8,15 +10,18 @@ window.addEventListener('load', function(){
                 if(xhr.readyState == 4 || xhr.readyState == 200 || xhr.readyState == 304){
                     xhr.responseText;
                     var body = document.getElementsByTagName("body")[0];
-                    var p = document.createElement("p");
-                    var textNode = document.createTextNode(xhr.responseText);
-                    p.appendChild(textNode);
-                    body.appendChild(p);
+                    //var p = document.createElement("p");
+                    //var textNode = document.createTextNode(xhr.responseText);
+                    //p.appendChild(textNode);
+                    //body.appendChild(p);
+                    var div = document.createElement("div");
+                    div.innerHTML = xhr.responseText;
+                    body.appendChild(div);
                 }
             }
             xhr.open("GET", "data.txt", true);
             xhr.send(null);
             return false;
         }
-    })
-})
+    
+    })();
