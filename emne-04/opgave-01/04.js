d32373bcb1cd2be4b46bdce2cd41d61077dc41ba
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-03-25 10:40:38 +0100

    commit

--- emne-04/opgave-01/04.js
+++ emne-04/opgave-01/04.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
     var productElement = document.querySelector('.product');
-    
+      
+    var dataset = productElement.dataset;
     // productElement.getAttribute('data-price');
     console.log(productElement);
 
@@ -10,4 +11,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 
    console.log(productElement.dataset.price );
    console.log(productElement.dataset.antal );
+
+   productElement.innerHTML =  " it cost " + dataset.price  +  ' for ' + dataset.antal + ' items' ;
 })
