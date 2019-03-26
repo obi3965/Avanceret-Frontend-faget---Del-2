d4d3ec6415466e2a58023f29e9bb67c575a9feae
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-03-26 08:49:03 +0100

    c

--- emne-02/main.js
+++ emne-02/main.js
@@ -1,30 +1,16 @@
 
-function all_items(){
+
        
-    var parentElement = document.querySelectorAll('.item');
-    //console.log(itemElement)
-    var itemElement_1 = document.querySelector(".item-title1");
-    var itemElement_2 = document.querySelector(".item-title2");
-      
-    for(var i = 0; i < parentElement.length; i++ ){
-         itemElement_1.innerHTML += "item1";
-         itemElement_2.innerHTML += "item2";
-         itemElement_1.classList.toggle(".item_1");
-         itemElement_2.classList.toggle(".item_1");
+    var allItems = document.querySelectorAll('.item');
+    
+    for(var i = 0; i < allItems.length; i++ ){
+         
+    allItems[i].addEventListener("click", function() {
+            allItems[i].classList.toggle(".item_1");
+          });
+         
+         
     }
         
-    itemElement_1.addEventListener('click', function(){
-        
-        console.log("item1")
-    })
-
-    itemElement_2.addEventListener('click', function(){
-        
-        console.log("item2")
-    })
     
-    console.log(parentElement)
 
-}
-
-all_items();
