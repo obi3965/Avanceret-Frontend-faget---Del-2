3dbb801b25dee75621a4a820cea1c849fe4f27a1
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-03-28 09:31:54 +0100

    commit

--- emne-02/main.js
+++ emne-02/main.js
@@ -1,11 +1,40 @@
+document.addEventListener('DOMContentLoaded', (event)=>{
 
-const parent = document.querySelectorAll(".item");
+  //FIRST EXAMPLE
+  let divs = document.querySelectorAll('.item');
 
-parent.addEventListener('click', (e) => {
-  e.target.classList.add('.item_1');
-  console.log(e.target);
-});
+  for (let index = 0; index < divs.length; ++index) {
+
+    divs[index].addEventListener('click', (e) => {
+      divs[index].classList.toggle('item_1');
+      console.log("index is :" + index);
+    })
+
+  }
+
+
+
+  //SECOND EXAMPLE
   
+  // let divs = document.querySelectorAll('.item');
+
+  // for (let index = 0; index < divs.length; ++index) {
+
+  //   divs[index].addEventListener('click', (e) => {
+      
+  //     if(divs[index].classList.contains('item_1')){
+  //       divs[index].classList.remove('item_1');
+        
+  //     }else{
+  //       divs[index].classList.add('item_1')
+  //     }
+  //     console.log("index is :" + index);
+  //   })
+
+  // }
+
+  
+})
         
     
 
