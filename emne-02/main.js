c19d0377f55a37f4a46220749537dd7de84c5f2b
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-03-28 08:11:12 +0100

    Update main.js

--- emne-02/main.js
+++ emne-02/main.js
@@ -1,16 +1,11 @@
 
+const parent = document.querySelectorAll(".item");
 
-       
-    var allItems = document.querySelectorAll('.item');
-    
-    for(var i = 0; i < allItems.length; i++ ){
-         
-    allItems[i].addEventListener("click", function() {
-            allItems[i].classList.toggle(".item_1");
-          });
-         
-         
-    }
+parent.addEventListener('click', (e) => {
+  e.target.classList.add('.item_1');
+  console.log(e.target);
+});
+  
         
     
 
